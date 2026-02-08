7066e89e2b0db1078e3165bb4d16b3d41956be32
Titi-shop <226829233+Titi-shop@users.noreply.github.com>
2026-02-09 00:34:37 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 import { useEffect, useMemo, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
