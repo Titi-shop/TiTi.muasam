4daab1b274772990123d1cda4639295b5a6694c2
Titi-shop <226829233+Titi-shop@users.noreply.github.com>
2026-02-28 08:20:14 +0700

    Update customerMenu.tsx

--- components/customerMenu.tsx
+++ components/customerMenu.tsx
@@ -14,6 +14,7 @@ import {
 } from "lucide-react";
 import { useTranslationClient as useTranslation } from "@/app/lib/i18n/client";
 import { useAuth } from "@/context/AuthContext";
+import { getPiAccessToken } from "@/lib/piAuth";
 
 export default function CustomerMenu() {
   const router = useRouter();
