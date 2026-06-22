466789267008475673b879ce32e5804d95d0e18d
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-06-23 01:56:49 +0700

    Update route.ts

--- app/api/debug/complete-payment/route.ts
+++ app/api/debug/complete-payment/route.ts
@@ -1,9 +1,7 @@
-// app/api/debug/complete-payment/route.ts
-
 import { NextResponse } from "next/server";
 import { completeA2UPayment } from "@/lib/pi/pi.a2u";
 
-export async function POST() {
+export async function GET() {
   await completeA2UPayment(
     "IOBhza8YXTZIcuyQWErZjbJUFJsY",
     "732729ccc696588e323d28bab2220ce26d3d24906f7050771c20d6c76d59127d"
