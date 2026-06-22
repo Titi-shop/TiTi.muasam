f852aeffcc4d44fd726afe5cb99247f5d64712e6
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-06-23 01:24:56 +0700

    Update route.ts

--- app/api/debug/pi-payment/route.ts
+++ app/api/debug/pi-payment/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
   try {
     const payment =
   await getA2UPayment(
-    "ISykghVaEeOCL1zJKdpEhVEBcKYb"
+    "IOBhza8YXTZIcuyQWErZjbJUFJsY"
   );
 
     console.log(
