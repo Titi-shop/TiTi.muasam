8d550c1ca18232a590c0fd93c90dbf46bde1f973
quản lý supabase <226829233+Titi-shop@users.noreply.github.com>
2026-07-11 15:44:29 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "standalone",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -30,28 +32,13 @@ const nextConfig = {
             key: "Content-Security-Policy",
 
             value: [
-              // base
               "default-src 'self' https: data: blob:;",
-
-              // PI SDK cần rất mở
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https: blob: data:;",
-
-              // API / websocket / pi
               "connect-src * https: wss: ws: data: blob:;",
-
-              // images
               "img-src * data: blob: https:;",
-
-              // css
               "style-src 'self' 'unsafe-inline' https:;",
-
-              // iframe payment pi browser
               "frame-src * https: data: blob:;",
-
-              // fonts
               "font-src * data: https:;",
-
-              // media
               "media-src * blob: data: https:;",
             ].join(" "),
           },
